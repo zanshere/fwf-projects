09d211cc781ff489dbc67a57f68dd465398d238e
zanshere <fauzan224321@gmail.com>
2025-09-04 09:19:27 +0700

    add: import lucide icons and gsap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,18 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
+import { createIcons, icons } from 'lucide';
+// import { gsap } from 'gsap';
+// import { ScrollTrigger } from 'gsap/ScrollTrigger';
+// // ScrollSmoother requires Scrolltrigger
+// import { ScrollSmoother } from 'gsap/ScrollSmoother';
+// import { TextPlugin } from 'gsap/TextPlugin';
 
+// Alpine JS
 window.Alpine = Alpine;
-
 Alpine.start();
+
+// Lucide Icons
+createIcons({ icons });
+
+// GSAP Initialization
+// gsap.registerPlugin(ScrollTrigger, ScrollSmoother, TextPlugin);
