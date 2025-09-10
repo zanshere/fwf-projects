0583986fe59702922240eadc998f049ba8776e05
GhanNgonten <ghananghafarullah@gmail.com>
2025-09-10 15:50:40 +0700

    create order table

---
+++ database/migrations/2025_09_04_015643_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('orders', function (Blueprint $table) {
+        $table->id();
+        $table->string('nama');
+        $table->string('email');
+        $table->enum('kategori', ['dewasa', 'anak', 'rombongan']);
+        $table->integer('jumlah');
+        $table->date('tanggal');
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
