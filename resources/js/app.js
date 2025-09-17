445d36e17abc4162b161058f82e3d21ada9fcc0a
zanshere <fauzan224321@gmail.com>
2025-09-17 20:53:22 +0700

    chore(deps): add Lucide Icons

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,10 @@
 import './bootstrap';
 import Alpine from 'alpinejs';
-
+import { createIcons, icons } from 'lucide'
 
 // Alpine JS
 window.Alpine = Alpine;
 Alpine.start();
 
+// Lucide Icons
+createIcons({ icons })
