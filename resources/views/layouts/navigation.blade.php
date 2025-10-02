bf42c195a48b5ddaf7ef868d4bb20a87097921c5
zanshere <fauzan224321@gmail.com>
2025-10-02 08:42:53 +0700

    style: add icon for login button

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -84,7 +84,7 @@
                     <!-- Login Button -->
                     <button onclick="window.location.href='{{ route('login') }}'"
                             class="bg-white/10 hover:bg-white/20 text-white px-4 py-2 rounded-lg transition-colors">
-                        <span>Login</span>
+                        <span><i data-lucide="user" class="w-5 h-5 pr-5"></i>Login</span>
                     </button>
                 @endauth
 
